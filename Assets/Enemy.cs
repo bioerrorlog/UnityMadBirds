0f24c4c22c024b36c29f90938e38ceca0a120e25
bioerrorlog <n.iijima.0x01@gmail.com>
2020-06-26 09:22:55 +0900

    Chapter14: Enemy

---
+++ Assets/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        bool didHitBird = collision.collider.GetComponent<Bird>() != null;
+        if (didHitBird)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
